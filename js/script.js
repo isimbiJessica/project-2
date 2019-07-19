fc81b09c8ce006139ae30ce6fcbdf859510ab791
isimbiJessica <jessicaritha@gmail.com>
2019-07-19 13:14:44 +0200

    adding a names

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 var dd = parseInt (prompt("enter the day you were born:"));
 var mm = parseInt (prompt("enter the month you were born in:"));
+var yy = parseInt (prompt("Enter the year you were born in:"));
 var cc = parseInt (prompt("enter the century:"));
+var gender = prompt("enter your Gender");
+var formular=(((cc/4 -2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd)%7);
+var day=Math.floor(formular);
+console.log("formular");
+console.log("gender");
+if(day===0&&gender==='male'){
+    console.log("your day name is Kwasi");
+}
+else if(day===1&&gender==='male'){
+        console.log("your day name is Kwasi");
+    }
+)
 
